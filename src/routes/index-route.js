a7cf8f134e18dd5d1a19ef037e62a430fa307076
Erick Leandro <j.erickleandro@gmail.com>
2022-04-06 02:10:48 -0300

    separando arquivos de rotas

---
+++ src/routes/index-route.js
@@ -0,0 +1,14 @@
+'use restrict';
+
+const express = require('express');
+const router = express.Router();
+
+
+const route = router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node API Example",
+        version: "0.0.1"
+    });
+});
+
+module.exports = router;
