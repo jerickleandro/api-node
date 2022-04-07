045110eb91e2b2957332a1591e2c9ce987fa6b1e
Erick Leandro <j.erickleandro@gmail.com>
2022-04-07 01:29:59 -0300

    Configurado mongoose + dotenv

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv/config');
 
 const app = express();
+
+mongoose.connect(process.env.CONECT_DB_STRING);
 
 //Carregar Rotas
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
